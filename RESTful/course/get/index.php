3d297ffaeb4afa67f8a1dbb852e4c3d25d5b7997
DGideas <dgideas@outlook.com>
2016-02-06 20:22:16 +0800

    design RESTful API

---
+++ RESTful/course/get/index.php
@@ -0,0 +1,15 @@
+<?php
+//Author:@DGideas
+//2016-02-06
+
+require("../../include.php");
+
+$requestHandle = new DGrst();
+$sqlHandle = new DGsql();
+
+/* DGRESTful API accept two types of request:
+    GET,
+    POST
+   them use global var $_GET and $_POST storage
+*/
+?>
