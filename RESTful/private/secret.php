6d08bde403f6c1c9d68c26cd4827b2af89b4e6c5
DGideas <dgideas@outlook.com>
2016-02-07 10:13:53 +0800

    Create secret.php

---
+++ RESTful/private/secret.php
@@ -0,0 +1,12 @@
+<?php
+//Author:@DGideas
+//2016-02-07
+
+// This global var is used to public/dgsql.php
+global $DGDATABASE = array();
+
+$DGDATABASE["location"] = "localhost";
+$DGDATABASE["username"] = "username";
+$DGDATABASE["password"] = "password";
+$DGDATABASE["dbname"] = "database";
+?>
