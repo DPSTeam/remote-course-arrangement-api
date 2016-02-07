0e6e1d496c6fd5e6c3707703ed78a7792d0dfb60
DGideas <dgideas@outlook.com>
2016-02-07 16:35:01 +0800

    sync

--- install/install.php
+++ install/install.php
@@ -2,5 +2,22 @@
 	require("../RESTful/include.php");
 	
 	$dbHandle = new DGsql();
-	$dbHandle->config_add("name1","config1");
+	
+	$config_params = array(
+		"dgrca_version" => "1.0",
+		"site_status" => "OK",
+		"site_addr" => "localhost",
+		"site_name" => "Another DGSite",
+		"site_lang" => "zh-CN",
+		"site_charset" => "utf-8",
+		"api_status" => "OK",
+		"api_query_freq" => "unlimited",
+		"api_query_times" => "1"
+		);
+	
+	// Insert sone data to `config`
+	foreach($config_params as $configName => $configValue)
+	{
+		$dbHandle->config_add($configName, $configValue);
+	}
 ?>
