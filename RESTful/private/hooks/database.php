6d1c1590ceb906c3dd7a5fca530c81e81a3c0057
DGideas <dgideas@outlook.com>
2016-02-07 21:07:10 +0800

    Delete database.php

--- RESTful/private/hooks/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Author:@DGideas
-//2016-02-07
-
-$GLOBALS["HOOKS_DATABASE"] = array();
-
-// Database prefix, for example: edu_users, edu_pages, etc.
-$GLOBALS["HOOKS_DATABASE"]["prefix"] = "edu_";
-?>
